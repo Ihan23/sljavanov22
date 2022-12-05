75b1c47d4bcaef65c6554037d1f4aec28cb5e583
ihan23 <pinkymaryt1@gmail.com>
2022-12-05 14:49:20 +0530

    "commit from eclipse"

---
+++ eclipse-workspace/sljavanov22/src/javaconcepts/javavariables.java
@@ -0,0 +1,18 @@
+package javaconcepts;
+
+public class javavariables {
+	int i=17;
+	static String str1 = "learning java";
+	int add(int a,int b) {
+		return(a+b);
+	}
+
+	public static void main(String[] args) {
+		javavariables obj= new javavariables();
+		System.out.println(obj.i);
+		System.out.println(obj.add(4,8));
+		// TODO Auto-generated method stub
+
+	}
+
+}
