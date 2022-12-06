fa45ae287573c5fdd608e19befb97f3563479c60
ihan23 <pinkymaryt1@gmail.com>
2022-12-06 23:07:50 +0530

    "commit for loop"

---
+++ javaconcepts/src/javaconcepts/forloop.java
@@ -0,0 +1,16 @@
+package javaconcepts;
+
+public class forloop {
+	// Program to print a text 5 times
+
+	
+	  public static void main(String[] args) {
+
+	    int n = 5;
+	    // for loop  
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.println("Java is fun");
+	    }
+	  }
+	}
+
